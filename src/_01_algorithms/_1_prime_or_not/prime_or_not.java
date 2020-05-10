eba28d9f01e66e2708ad4e3c05fd28387eb660b0
root <root@7e6df5fff869>
2020-05-10 22:59:59 +0000

    prime or not

---
+++ src/_01_algorithms/_1_prime_or_not/prime_or_not.java
@@ -0,0 +1,23 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class prime_or_not {
+	public static void main(String[] args) {
+		
+		String prime = JOptionPane.showInputDialog("Pick a number under 100...");
+		
+		for(int i=0; i<100; i++) {
+			if() {
+				JOptionPane.showMessageDialog(null, "It is a prime number");
+			}
+		else {
+		JOptionPane.showMessageDialog(null, "It is not a prime number");	
+		}
+		}
+		
+		
+		
+		
+	}
+}
