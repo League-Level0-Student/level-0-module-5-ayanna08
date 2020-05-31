3cd1273acc8a145ce0405055a0baf4eb27efab8f
root <root@6568a48fdcae>
2020-05-31 22:55:18 +0000

    for_loop_gauntlet

--- src/_02_nested_loops/_3_for_loop_gauntlet/for_loop_gauntlet.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/for_loop_gauntlet.java
@@ -3,6 +3,8 @@ package _02_nested_loops._3_for_loop_gauntlet;
 public class for_loop_gauntlet {
 	public static void main(String[] args) {
 
+//Single For Loops
+		
 //1		 
 		for (int i = 0; i < 101; i++) {
 			System.out.println(i);
@@ -48,12 +50,51 @@ public class for_loop_gauntlet {
 		
 //7 
 		for (int i = 2008; i < 2021; i++) {
-			System.out.println("In, " + i + " I was"  );
+			System.out.println("In, " + i + " I was " + (i - 2008) + " years old." );
 		}
-			
 		
 
+//Nested For Loops
 		
+//1
+		for (int j = 0; j < 3; j++) {
+			for (int i = 0; i < 3; i++) {
+				System.out.println(j + " " + i);
+			}
+		}
+		
+//2
+		for (int j = 0; j < 7; j+=3) {
+			
+			for (int i = 1; i < 4; i++) {
+				System.out.print(i + j + " ");
+	}
+			System.out.println();
+}
+
+		
+//3
+		for (int j = 0; j < 100; j+=10) {
+			
+			for (int i = 1; i < 11; i++) {
+				System.out.print(i + j + " ");
+	}
+			System.out.println();
+}
+
+//4 
+		for (int j = 0; j < 7; j++) {
+			for (int i = 0; i < j; i++) {
+				System.out.print("*" + " ");
+			}
+			System.out.println();
+		}
+		
+
+// **BONUS**
+		for (int i = 0; i < 101; i++) {
+			System.out.println(100 - i);
+		}
 		
 	}
 }
